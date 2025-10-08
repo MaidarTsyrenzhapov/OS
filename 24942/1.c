581df74caba1753f862e844fa24b2fdd5f9cc5ff
Yhaatai <x.uuuuuu.d@gmail.com>
2025-10-08 17:44:50 +0700

    Добавлены комментарии к программе 1.c (работа с процессами)

--- 24942/1.c
+++ 24942/1.c
@@ -9,6 +9,7 @@
 int main(int argc, char *argv[]){
     int opt;
     
+    // если программа запущена без аргументов просто типо ./program
     if (argc == 1){
         printf("Использование: %s [опции]\n", argv[0]);
         printf("Опции:\n");
@@ -23,67 +24,72 @@ int main(int argc, char *argv[]){
     }
     
     while ((opt = getopt(argc, argv, "ispucdv")) != -1){
-        switch(opt){
-            case 'i':
+            switch(opt){
+                case 'i': //опция -i
                 printf("=== Идентификаторы пользователя и группы ===\n");
-                printf("Реальный ID пользователя: %d\n", getuid());
-                printf("Эффективный ID пользователя: %d\n", geteuid());
-                printf("Реальный ID группы: %d\n", getgid());
+                printf("Реальный ID пользователя: %d\n", getuid()); // кто запустил
+                printf("Эффективный ID пользователя: %d\n", geteuid()); //от кого (имя) запущено
+                printf("Реальный ID группы: %d\n", getgid()); // то же самое для групп
                 printf("Эффективный ID группы: %d\n", getegid());
                 break;
-            case 's':
+                
+            case 's': //опция -s
                 printf("=== Становление лидером группы ===\n");
-                if (setpgid(0, 0) == 0) {
-                    printf("Стал лидером группы процессов. Новый PGID: %d\n", getpgrp());
+                if (setpgid(0, 0) == 0){ // сделать процесс лидером 
+                    printf("Стал лидером группы процессов. Новый PGID: %d\n", getpgrp()); // айди группы процессов
                 } else {
                     perror("Ошибка при установке лидера группы");
                 }
                 break;
-            case 'p':
+            case 'p': //опция -p
                 printf("=== Идентификаторы процесса ===\n");
-                printf("ID процесса: %d\n", getpid());
-                printf("ID родительского процесса: %d\n", getppid());
-                printf("ID группы процессов: %d\n", getpgrp());
+                printf("ID процесса: %d\n", getpid()); //айди текущего процесса
+                printf("ID родительского процесса: %d\n", getppid()); //айди родителя
+                printf("ID группы процессов: %d\n", getpgrp()); // айди группы процессов
                 break;
-            case 'u':
+                
+            case 'u': //опция -u
                 printf("=== Ulimit ===\n");
                 struct rlimit rlim;
-                if (getrlimit(RLIMIT_FSIZE, &rlim) == 0) {
-                    printf("Текущий ulimit: %ld\n", (long)rlim.rlim_cur);
+                if (getrlimit(RLIMIT_FSIZE, &rlim) == 0){ //лимит на размер файлов
+                    printf("Текущий ulimit: %ld\n", (long)rlim.rlim_cur); //текущее значение лимита
                 } else {
-                    perror("Ошибка при получении ulimit");
+                    perror("Ошибка при получении ulimit"); 
                 }
                 break;
-            case 'c':
+                
+            case 'c': //опция -c
                 printf("=== Размер core-файла ===\n");
                 struct rlimit core_lim;
-                if (getrlimit(RLIMIT_CORE, &core_lim) == 0) {
-                    printf("Максимальный размер core-файла: %ld байт\n", (long)core_lim.rlim_cur);
+                if (getrlimit(RLIMIT_CORE, &core_lim) == 0){ //лимит на размер core-файлов
+                    printf("Максимальный размер core-файла: %ld байт\n", (long)core_lim.rlim_cur); 
                 } else {
                     perror("Ошибка при получении размера core-файла");
                 }
                 break;
-            case 'd':
+                
+            case 'd': //опция -d
                 printf("=== Текущая директория ===\n");
-                char cwd[1024];
-                if (getcwd(cwd, sizeof(cwd)) != NULL) {
+                char cwd[1024]; //создаем буфера 1024 байта 
+                if (getcwd(cwd, sizeof(cwd)) != NULL){ //здесь путь к директории
                     printf("Текущая директория: %s\n", cwd);
                 } else {
                     perror("Ошибка при получении текущей директории");
                 }
                 break;
-            case 'v':
+                
+            case 'v': //опция -v
                 printf("=== Переменные среды ===\n");
                 extern char **environ;
                 for (char **env = environ; *env != NULL; env++) {
                     printf("%s\n", *env);
                 }
                 break;
-            case '?':
-                printf("Неизвестная опция: %c\n", optopt);
+                
+            case '?': //Неизвестная опция
+                printf("Неизвестная опция: %c\n", opt);
                 break;
         }
     }
     return 0;
 }
-// Последнее обновление: Ср 08 окт 2025 17:36:51 +07
