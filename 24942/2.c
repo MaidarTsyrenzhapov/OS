ead4f296d878be7b978689843b5d8c440073bee9
Yhaatai <x.uuuuuu.d@gmail.com>
2025-10-08 17:56:36 +0700

    Добавлены комментарии к программе 2.c (время в Калифорнии)

--- 24942/2.c
+++ 24942/2.c
@@ -3,16 +3,14 @@
 #include <stdlib.h>
 
 int main() {
-    time_t t = time(NULL);
+    time_t t = time(NULL); //время в секундах с 01.01.1970
     
-    printf("UTC время: %s", asctime(gmtime(&t)));
-    printf("Локальное время: %s", ctime(&t));
+    printf("UTC время: %s", asctime(gmtime(&t)));//преобр в tm UTC а потом в читаеую строку
+    printf("Локальное время: %s", ctime(&t)); //локальное время
     
-    setenv("TZ", "PST8PDT", 1);
-    tzset();
-    printf("Калифорния (PST/PDT): %s", ctime(&t));
+    setenv("TZ", "PST8PDT", 1); //-8 время от UTC(PST) а потом в PDT это +1 от PST
+    tzset(); //без него не работает
+    printf("Калифорния (PST/PDT): %s", ctime(&t));//вывод
     
     return 0;
 }
-// Последнее обновление: Ср 08 окт 2025 17:36:52 +07
-// Обновлено: Ср 08 окт 2025 17:53:45 +07
